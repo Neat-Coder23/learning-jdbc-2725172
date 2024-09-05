15df9b89809039cf8e05e59e0c0614c305aacba5
Frank P Moley III <frankmoley@gmail.com>
2024-09-05 03:06:58 +0000

    files for 02_08

--- learning-jdbc/src/main/java/com/frankmoley/lil/data/dao/ServiceDao.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/data/dao/ServiceDao.java
@@ -21,6 +21,7 @@ public class ServiceDao implements Dao<Service, UUID>{
   private static final String GET_BY_ID = "select service_id, name, price from wisdom.services where service_id = ?";
   private static final String CREATE = "insert into wisdom.services (service_id, name, price) values (?,?,?)";
   private static final String UPDATE = "update wisdom.services set name = ?, price = ? where service_id = ?";
+  private static final String DELETE = "delete from wisdom.services where service_id = ?";
 
   @Override
   public Service create(Service entity) {
@@ -53,7 +54,22 @@ public class ServiceDao implements Dao<Service, UUID>{
 
   @Override
   public void delete(UUID id) {
-    // TODO Auto-generated method stub
+    Connection connection = DatabaseUtils.getConnection();
+    try{
+      connection.setAutoCommit(false);
+      PreparedStatement statement = connection.prepareStatement(DELETE);
+      statement.setObject(1, id);
+      statement.executeUpdate();
+      connection.commit();
+      statement.close();
+    } catch (SQLException e) {
+      try {
+        connection.rollback();
+      } catch (SQLException sqle) {
+        DatabaseUtils.handleSqlException("ServiceDao.delete.rollback", sqle, LOGGER);
+      }
+      DatabaseUtils.handleSqlException("ServiceDao.delete", e, LOGGER);
+    }
     
   }
 
