489aa2660f893b91a2e7bfca95735729e5a9d6e6
Frank P Moley III <frankmoley@gmail.com>
2024-09-05 05:02:22 +0000

    files for 03_03

--- learning-jdbc/src/main/java/com/frankmoley/lil/App.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/App.java
@@ -62,5 +62,11 @@ public class App {
 
         System.out.println("\n\n*** LIMIT ***");
         serviceDao.getAllLimit(2).forEach(System.out::println);
+
+        System.out.println("\\*** PAGED ***");
+        for(int i=1;i<11;i++){
+            System.out.println("Page number: " + i);
+            customerDao.getAllPaged(i, 10).forEach(System.out::println);
+        }
     }
 }
